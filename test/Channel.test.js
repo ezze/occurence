1852f0d3841ffe39314ff6ca56042796c2eaa920
Dmitriy Pushkov <ezze@ezze.org>
2017-05-03 10:02:07 +0300

    More tests for event mixin are written.

--- test/Channel.test.js
+++ test/Channel.test.js
@@ -7,7 +7,8 @@ describe('Channel', () => {
     it('add event listener and fire event', () => {
         let handled = false;
         const channel = new Channel();
-        channel.on('event', () => handled = true);
+        const listener = () => handled = true;
+        channel.on('event', listener);
         channel.fire('event');
         handled.should.be.true;
     });
@@ -22,5 +23,113 @@ describe('Channel', () => {
         channel.off('event', listener);
         channel.fire('event');
         count.should.be.equal(2);
+    });
+
+    it('add few event listeners and fire event', () => {
+        let result = '';
+        const channel = new Channel();
+        const listener1 = () => result += '1';
+        const listener2 = () => result += '2';
+        const listener3 = () => result += '3';
+        channel.on('event', listener1);
+        channel.on('event', listener2);
+        channel.on('event', listener3);
+        channel.fire('event');
+        result.should.equal('123');
+    });
+
+    it('remove all event listeners', () => {
+        let result = '';
+        const channel = new Channel();
+        const listener1 = () => result += '1';
+        const listener2 = () => result += '2';
+        const listener3 = () => result += '3';
+        channel.on('event', listener1);
+        channel.on('event', listener2);
+        channel.on('event', listener3);
+        channel.fire('event');
+        channel.off('event');
+        channel.on('event', listener1);
+        channel.on('event', listener2);
+        channel.on('event', listener3);
+        result.should.equal('123');
+    });
+
+    it('remove all listeners of event that has no registered listeners', () => {
+        const channel = new Channel();
+        channel.off.bind(channel, 'event').should.throw(TypeError);
+    });
+
+    it('remove listener of event that has no registered listeners', () => {
+        const channel = new Channel();
+        const listener = () => console.log('hello world');
+        channel.on('event', listener);
+        channel.off.bind(channel, 'anotherEvent', listener).should.throw(TypeError);
+    });
+
+    it('remove listener of event having registered listeners but not this one', () => {
+        const channel = new Channel();
+        const listener1 = () => console.log('hello world');
+        const listener2 = () => console.log('remove me');
+        channel.on('event', listener1);
+        channel.off.bind(channel, 'event', listener2).should.throw(TypeError);
+    });
+
+    it('fire event without registered listeners', () => {
+        let handled = false;
+        const channel = new Channel();
+        const listener = () => handled = true;
+        channel.fire('event');
+        handled.should.be.false;
+        channel.on('anotherEvent', listener);
+        channel.fire('event');
+        handled.should.be.false;
+    });
+
+    it('interrupt event listeners\' execution chain', () => {
+        let result = '';
+        const channel = new Channel();
+        const listener1 = () => result += '1';
+        const listener2 = () => {
+            result += '2';
+            return false;
+        };
+        const listener3 = () => result += '3';
+        channel.on('event', listener1);
+        channel.on('event', listener2);
+        channel.on('event', listener3);
+        channel.fire('event');
+        result.should.equal('12');
+    });
+
+    it('event name and parameters as event listener arguments', () => {
+        const channel = new Channel();
+        const eventName = 'event';
+        const eventParams = {
+            item: 1,
+            message: 'hello world'
+        };
+        const listener = (name, params) => {
+            name.should.equal(eventName);
+            params.should.deep.equal(eventParams);
+        };
+        channel.on(eventName, listener);
+        channel.fire(eventName, eventParams);
+    });
+
+    it('pass additional options to event listeners', () => {
+        const channel = new Channel();
+        const eventOptions = {
+            item: 1,
+            message: 'hello world'
+        };
+        const listener = (name, params, options) => {
+            options.should.deep.equal(eventOptions);
+        };
+        channel.on('event', listener, eventOptions);
+        channel.fire('event');
+        channel.fire('event', {
+            param: 345
+        });
     });
 });
