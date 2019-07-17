46e4dd807ff72fdc9d84d5da17f72742cabdaf74
Dmitriy Pushkov <ezze@ezze.org>
2019-07-18 00:51:27 +0300

    ESLint and code style are updated.

--- src/getChannel.js
+++ src/getChannel.js
@@ -3,9 +3,9 @@ import Channel from './Channel';
 const channels = {};
 
 export default function getChannel(name) {
-    name = name || 'application';
-    if (!channels[name]) {
-        channels[name] = new Channel();
-    }
-    return channels[name];
+  name = name || 'application';
+  if (!channels[name]) {
+    channels[name] = new Channel();
+  }
+  return channels[name];
 }
